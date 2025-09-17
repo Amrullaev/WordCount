4a001b67c451d0fe67ea8a66c2fa74e3f658177c
Uchqun Amrullayev <u.amrullayev@openweb.uz>
2025-09-17 14:37:06 +0500

    End version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "uz.appvero.wordCount"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
