8f62c8d1f6917b9241c052a504de88d1fcc185f0
myNout <amrullayevuchqun@gmail.com>
2025-12-10 13:52:07 +0500

    End version 1.0.4 Add space to labels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "uz.appvero.wordCount"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
